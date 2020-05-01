812c8bc87fd5d1d05128bcd3a2910354a5d4ac40
B G Vinayak <bhatvinayak94@gmail.com>
2020-05-01 11:59:38 +0530

    initial commit

---
+++ msg.py
@@ -0,0 +1,32 @@
+
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By 
+import time 
+  
+# Replace below path with the absolute path 
+# to chromedriver in your computer 
+driver = webdriver.Chrome('/Users/B G Vinayak/Downloads/chromedriver') 
+  
+driver.get("https://web.whatsapp.com/") 
+wait = WebDriverWait(driver, 600) 
+  
+# Replace 'Friend's Name' with the name of your friend  
+# or the name of a group  
+target = '"Hale-hudru"'
+  
+# Replace the below string with your own message 
+string = "Message sent using Python!!!"
+  
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located(( 
+    By.XPATH, x_arg))) 
+group_title.click() 
+inp_xpath = '//div[@class="_2S1VP copyable-text selectable-text"][@contenteditable="true"][@data-tab="1"]'
+input_box = driver.find_element_by_xpath(inp_xpath)
+for i in range(10): 
+    input_box.send_keys(string) 
+    input_box.send_keys(Keys.ENTER)
+    time.sleep(1)
